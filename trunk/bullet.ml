cf905fcec4bc340d14578e6155e0c1f0fd332524
xiangyao1989@gmail.com <xiangyao1989@gmail.com@53f70ae7-0bce-c439-0189-13a457c33da1>
2012-11-28 01:22:19 +0000

    update positions for bullets

--- trunk/bullet.ml
+++ trunk/bullet.ml
@@ -6,25 +6,48 @@ class bullet =
 	object (self)
 
 	val mutable direction_of_the_body = 0;
-	val mutable current_position_x = 0;
-	val mutable current_position_y = 0;
+	val mutable x_position = 0;
+	val mutable y_position = 0;
 	val mutable distance = 0;
 
+	val mutable start_x_position = 0;
+	val mutable start_y_position = 0;
+	val mutable pi = 4. *. atan 1.
 
-	method get_pos_x = current_position_x
+	method get_pos_x = x_position
 
-	method get_pos_y = current_position_y
+	method get_pos_y = y_position
 
 	method get_direction = direction_of_the_body
 
-	method get_distance = distance
+	method set_x_position x = x_position <- x
 
-	method set_x_position x = current_position_x <- x
-
-	method set_y_position y = current_position_y <- y 
+	method set_y_position y = y_position <- y 
 
 	method set_direction dire = direction_of_the_body <- dire
 
 	method set_distance dis = distance <- dis;
+
+	method move = 
+		begin
+			y_position <- y_position + int_of_float(500. *. (tan (float_of_int(direction_of_the_body) *. pi /. 180.)));
+			x_position <- x_position + int_of_float(500. *. (1. /. (tan (float_of_int(direction_of_the_body) *. pi /. 180.))));
+		end
+
+	method cal_distance x1 y1 x2 y2 = 
+		int_of_float(sqrt(float_of_int((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2))))
+
+	method check_reach_distance =
+		let dis = self#cal_distance x_position y_position start_x_position start_y_position in
+		if  dis >= distance
+		then true
+		else false
+
+
+	method check_hit_wall = 
+		if x_position > 1000 || x_position < 0 || y_position > 1000 || y_position < 0
+		then true
+		else false
+
 
 end;;
